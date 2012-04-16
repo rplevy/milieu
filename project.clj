c139962a181e1e1bd2aac3a82ee01f16d14577ef
Rob Levy <robert.levy@drakerlabs.com>
2012-04-16 11:42:19 -0400

    use new version of swiss arrows

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-yaml "0.3.1"]
-                 [swiss-arrows "0.1.0"]]
+                 [swiss-arrows "0.3.0"]]
   :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
