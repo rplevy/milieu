cc5fcdee5cd9877ad0668bb8cac373513dc3e01d
Rob Levy <robert.levy@drakerlabs.com>
2012-06-22 17:01:33 -0400

    upgrade midje

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-yaml "0.3.1"]
                  [swiss-arrows "0.3.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0-RC1"]]}}
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
